000da05604b09f1ee566b2d587db76e6006b6f07
Akber Malik <akber@Akbers-MBP-2.lan>
2014-11-26 15:33:27 -0800

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+  	templateUrl: 'partials/landing_page.html', 
+  	controller: 'LandingPageController'
+  });
+
+
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('LandingPageController', [function(){
+
+ 
+
+  }]);
