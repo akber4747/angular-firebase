ecd2440b909ef6b058044e650a77dd374923ae39
Akber Malik <akber@Akbers-MacBook-Pro-2.local>
2015-03-14 13:49:30 -0700

    textMessageService

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,43 @@
 angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function(){
 
- 
+ }])
+   .controller('WaitlistController', ['$scope', 'partyService', function($scope, partyService){
+      // Connet $scope.parties to live Firebase data
+   	$scope.parties = partyService.parties; 
+      // Object to store data from the waitlist form
+      // adding done property to this object (also change in SaveParty funciton)
+   	$scope.newParty = {name: '', phone : '', size: '', done: false, notified: 'No'};
+      // Function to save new parties to the waitlist
+   	$scope.saveParty = function(){
+         partyService.saveParty($scope.newParty);
+         $scope.newParty = {name: '', phone : '', size: '', done: false, notified: 'No'};
+   	};
+      // Function to send a text message to a party
+      $scope.sendTextMessage = function(party){
 
-  }]);
+      };
+      }])
+      .controller('AuthController',['$scope', 'authService', function($scope, authService){
+
+
+         // Object bound to inputs on the register and login pages
+         $scope.user = {email:'', password: ''};
+         // method to register new user using authService (services.js)
+         $scope.register = function(){
+            authService.register($scope.user);
+
+         };
+         // method to login user using authService (services.js)
+         $scope.login = function(){
+            authService.login($scope.user);
+         };
+         // method to logout user using authService (services.js)
+
+         $scope.logout = function(){
+            authService.logout();
+            // auth.$logout();
+            // $location.path('/#');
+            // // redirect users to landing page
+         };
+      }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,71 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+// .value('FIREBASE_URL', 'http://waitandeat-akber.firebaseio.com/parties')
+.value('FIREBASE_URL', 'http://waitandeat-akber.firebaseio.com/')
+.factory('partyService', function($firebase, FIREBASE_URL){
+	var partiesRef = new Firebase(FIREBASE_URL + '/parties');  	
+   	var parties = $firebase(partiesRef);
+
+   	var partyServiceObject = {
+   		saveParty: function(party){
+   			parties.$add(party);
+   			
+   		}
+
+   	};
+   	return partyServiceObject;
+})
+.factory('textMessageService', function(FIREBASE_URL){
+     var textMessageRef = new Firebase(FIREBASE_URL + '/textMessages');
+     var textMessages = $firebase(textMessageRef);
+     // new object
+     var newTextMessage = {
+        phoneNumber: party.phone,
+        size: party.size,
+        name: party.name
+     };
+     textMessages.$add(newTextMessage);
+     party.notified = 'Yes';
+     $scope.parties.$save(party.$id);	
+})
+.factory('authService', function($firebaseSimpleLogin, $location, $rootScope, FIREBASE_URL){
+	var AuthRef = new Firebase(FIREBASE_URL);
+    var auth = $firebaseSimpleLogin(AuthRef);
+	var authServiceObject = {
+		register: function(user){
+
+            auth.$createUser(user.email, user.password).then(function(data){
+               // .then handles success
+                  console.log(data);
+                  authServiceObject.login(user);
+
+            });				
+		
+		},		
+		login: function(user){
+			auth.$login('password', user).then(function(data){
+               console.log(data);
+               $location.path('/waitlist');
+               //redirect users to /waitlist 
+            });
+		},
+		logout: function(){
+			auth.$logout();
+            $location.path('/#');
+            // redirect users to landing page
+		}
+
+	};
+	$rootScope.$on("$firebaseSimpleLogin:login", function(e, user){
+		console.log("User " + user.id + " successfully logged in!");
+		// Save currentUser on our rootScope
+		$rootScope.currentUser = user;
+	});
+	$rootScope.$on("$firebaseSimpleLogin:logout", function(){
+		// set currentUser to null (user not logged in)
+		$rootScope.currentUser = null;
+	});	
+	return authServiceObject;
+});
